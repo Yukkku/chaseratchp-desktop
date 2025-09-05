0026223a4ec621508f8b2dfbc1c7f657a9c8d460
Yukkku <yukku.scratcher@gmail.com>
2025-09-05 13:40:22 +0900

    なんかサンドボックスが邪魔で上手くいかなかったので通すだけにする

--- src-preload/editor.js
+++ src-preload/editor.js
@@ -92,99 +92,33 @@ window.addEventListener('keydown', (e) => {
   if (e.ctrlKey && e.key === 'F12') ipcRenderer.send('debug:open-devtools');
 });
 
-/** @type {Map<string, [() => void, (info: string) => void, (info: string) => void]>} */
-const sessions = new Map();
-
-contextBridge.exposeInMainWorld('createCHaserSession', async (host, port, name) => {
-  /** @type {string | null} */
-  const id = await ipcRenderer.invoke('chaser:connect', host, port, name);
-  if (id == null) return null;
-  /** @type {Set<() => unknown>} */
-  const closeListeners = new Set();
-  /** @type {Set<(info: string) => unknown>} */
-  const turnListeners = new Set();
-  /** @type {0|1|2|3} */
-  let status = 0;
-  /** @type {string | null} */
-  let skinfo = null;
-  /** @type {((info: string | null) => void) | null} */
-  let presolver = null;
-  sessions.set(id, [() => {
-    sessions.delete(id);
-    status = 3;
-    presolver?.(null);
-    for (const listener of closeListeners) try {
-      listener();
-    } catch(e) {
-      console.error(e);
-    }
-  }, (info) => {
-    if (status === 3) return;
-    if (status === 2) {
-      skinfo = info;
-      return;
-    }
-    if (status === 1) throw new Error();
-    status = 1;
-    for (const listener of turnListeners) try {
-      listener(info);
-    } catch(e) {
-      console.error(e);
-    }
-  }, (info) => {
-    status = 0;
-    presolver?.(info);
-    if (skinfo) {
-      skinfo = null;
-      status = 1;
-      for (const listener of turnListeners) try {
-        listener(info);
-      } catch(e) {
-        console.error(e);
-      }
-    }
-  }]);
-  return {
-    close: () => {
-      if (status === 3) return;
-      sessions.delete(id);
-      status = 3;
-      presolver?.(null);
-      ipcRenderer.send('chaser:close', id);
-    },
-    /**
-     * @param {string} command
-     * @returns {Promise<string | null>}
-     */
-    send: (command) => {
-      if (status !== 1) return;
-      status = 2;
-      ipcRenderer.send('chaser:send', id, command);
-      return new Promise(resolve => {
-        presolver = resolve;
-      });
-    },
-    get isMyturn() { return status === 1; },
-    get isClosed() { return status === 3; },
-    /** @param {() => unknown} listener */
-    onClose: (listener) => { closeListeners.add(listener); },
-    /** @param {() => unknown} listener */
-    offClose: (listener) => { closeListeners.delete(listener); },
-    /** @param {(info: string) => unknown} listener */
-    onMyturn: (listener) => { turnListeners.add(listener); },
-    /** @param {(info: string) => unknown} listener */
-    offMyturn: (listener) => { turnListeners.delete(listener); },
-  };
-});
-
-ipcRenderer.on('chaser:close', (_e, sessionid) => {
-  sessions.get(sessionid)?.[0]();
-});
-
-ipcRenderer.on('chaser:myturn', (_e, sessionid, info) => {
-  sessions.get(sessionid)?.[1](info);
-});
-
-ipcRenderer.on('chaser:turnend', (_e, sessionid, info) => {
-  sessions.get(sessionid)?.[2](info);
+contextBridge.exposeInMainWorld('chaserClientPreload', {
+  /**
+   * @param {string} host
+   * @param {number} port
+   * @param {string} name
+   * @returns {Promise<string | null>}
+   */
+  connect: (host, port, name) => ipcRenderer.invoke('chaser:connect', host, port, name),
+  /**
+   * @param {string} sessionid
+   * @param {string} command
+   */
+  send: (sessionid, command) => ipcRenderer.send('chaser:send', sessionid, command),
+  /**
+   * @param {string} sessionid
+   */
+  close: (sessionid) => ipcRenderer.send('chaser:close', sessionid),
+  /**
+   * @param {(sessionid: string) => unknown} listener
+   */
+  onClose: (listener) => { ipcRenderer.on('chaser:close', (_e, id) => listener(id)); },
+  /**
+   * @param {(sessionid: string, info: string) => unknown} listener
+   */
+  onMyturn: (listener) => { ipcRenderer.on('chaser:myturn', (_e, id, info) => listener(id, info)); },
+  /**
+   * @param {(sessionid: string, info: string) => unknown} listener
+   */
+  onTurnend: (listener) => { ipcRenderer.on('chaser:turnend', (_e, id, info) => listener(id, info)); },
 });
